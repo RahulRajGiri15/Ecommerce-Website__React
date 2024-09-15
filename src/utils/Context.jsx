4f557d0d2507b206782f3f506ead81066e165b35
RahulRajGiri15 <160770727+RahulRajGiri15@users.noreply.github.com>
2024-09-15 18:24:51 +0530

    Working model of project for online deployment

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,51 +1,7 @@
-//This is the data till we were improting the data from the fake store API 
-
-// import axios from "./axios"
-// import React, { createContext, useEffect, useState } from 'react'
-
-
-// export const ProductContent = createContext();
-
-
-
-// const Context = (props) => {
-
-//     const [products , setproducts] = useState(null);
-
-//      ///////here we are importing the data from api and make it available through out the program
-//     const getproducts = async () => {
-//         try{
-//             const {data} = await axios ("/products");
-//             setproducts(data);
-//             //console.log(data);
-//         } catch(error){
-//             console.log(error);
-//         }
-//     };
-//     useEffect (() => {
-//         getproducts();
-//     }, []);
-//     return (
-//         <ProductContent.Provider value ={[products,setproducts]}>
-//             {props.children}
-//         </ProductContent.Provider> 
-
-    
-
-//   );
-// };
-
-// export default Context
-
-
-
-/////This is the part where we are creating our own dataBases
-
-
+////This is the data till we were improting the data from the fake store API 
 
 import axios from "./axios"
 import React, { createContext, useEffect, useState } from 'react'
-
 
 
 export const ProductContent = createContext();
@@ -54,23 +10,21 @@ export const ProductContent = createContext();
 
 const Context = (props) => {
 
-    const [products , setproducts] = useState(JSON.parse(localStorage.getItem("products")) ||null);
+    const [products , setproducts] = useState(null);
 
      ///////here we are importing the data from api and make it available through out the program
-    // const getproducts = async () => {
-    //     try{
-    //         const {data} = await axios ("/products");
-    //         setproducts(data);
-    //         //console.log(data);
-    //     } catch(error){
-    //         console.log(error);
-    //     }
-    // };
-    // console.log(products);
-    // useEffect (() => {
-    //     getproducts();
-    // }, []);
-
+    const getproducts = async () => {
+        try{
+            const {data} = await axios ("/products");
+            setproducts(data);
+            //console.log(data);
+        } catch(error){
+            console.log(error);
+        }
+    };
+    useEffect (() => {
+        getproducts();
+    }, []);
     return (
         <ProductContent.Provider value ={[products,setproducts]}>
             {props.children}
@@ -82,3 +36,49 @@ const Context = (props) => {
 };
 
 export default Context
+
+
+
+/////This is the part where we are creating our own dataBases
+
+
+
+// import axios from "./axios"
+// import React, { createContext, useEffect, useState } from 'react'
+
+
+
+// export const ProductContent = createContext();
+
+
+
+// const Context = (props) => {
+
+//     const [products , setproducts] = useState(JSON.parse(localStorage.getItem("products")) ||null);
+
+//      ///////here we are importing the data from api and make it available through out the program
+//     // const getproducts = async () => {
+//     //     try{
+//     //         const {data} = await axios ("/products");
+//     //         setproducts(data);
+//     //         //console.log(data);
+//     //     } catch(error){
+//     //         console.log(error);
+//     //     }
+//     // };
+//     // console.log(products);
+//     // useEffect (() => {
+//     //     getproducts();
+//     // }, []);
+
+//     return (
+//         <ProductContent.Provider value ={[products,setproducts]}>
+//             {props.children}
+//         </ProductContent.Provider> 
+
+    
+
+//   );
+// };
+
+// export default Context
